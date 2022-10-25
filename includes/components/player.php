7ec6d228793424fe11436c82a855d3c5a1863440
unknown <israel-victor@bol.com.br>
2022-10-25 01:28:45 -0300

    added playerIcon scss, added playerIcon html

---
+++ includes/components/player.php
@@ -0,0 +1,122 @@
+<section class="playerArea">
+    <div class="container-xxl container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+
+                <div class="titleArea">
+                    <small>Veja</small>
+                    <h1>Nosso time</h1>
+                </div>
+
+            </div>
+
+
+            <!-- PLAYER NAME 1 -->
+            <div class="col-lg-3">
+                <div class="playerIcon active">
+
+                    <div class="icon">
+                        <img src="src/img/icons/top.png" alt="">
+                    </div>
+
+                    <div class="name">
+                        <div class="nameEffect">
+                            <img src="src/img/team-nick-effect.png" alt="">
+                        </div>
+
+                        <span>SooH Biscoit0</span>
+                    </div>
+
+                </div>
+
+                <!-- PLAYER NAME 2 -->
+                <div class="playerIcon">
+
+                    <div class="icon">
+                        <img src="src/img/icons/jg.png" alt="">
+                    </div>
+
+                    <div class="name">
+                        <div class="nameEffect">
+                            <img src="src/img/team-nick-effect.png" alt="">
+                        </div>
+
+                        <span>SooH Croco</span>
+                    </div>
+
+                </div>
+
+                <!-- PLAYER NAME 3 -->
+                <div class="playerIcon">
+
+                    <div class="icon">
+                        <img src="src/img/icons/mid.png" alt="">
+                    </div>
+
+                    <div class="name">
+                        <div class="nameEffect">
+                            <img src="src/img/team-nick-effect.png" alt="">
+                        </div>
+
+                        <span>SooH Vonor</span>
+                    </div>
+
+                </div>
+
+                <!-- PLAYER NAME 4 -->
+                <div class="playerIcon">
+
+                    <div class="icon">
+                        <img src="src/img/icons/adc.png" alt="">
+                    </div>
+
+                    <div class="name">
+                        <div class="nameEffect">
+                            <img src="src/img/team-nick-effect.png" alt="">
+                        </div>
+
+                        <span>SooH Greed</span>
+                    </div>
+                </div>
+
+
+                <!-- PLAYER NAME 5 -->
+                <div class="playerIcon">
+
+                    <div class="icon">
+                        <img src="src/img/icons/sup.png" alt="">
+                    </div>
+
+                    <div class="name">
+                        <div class="nameEffect">
+                            <img src="src/img/team-nick-effect.png" alt="">
+                        </div>
+
+                        <span>SooH Lari</span>
+                    </div>
+
+                </div>
+
+                <!-- PLAYER NAME 6 -->
+                <div class="playerIcon">
+
+                    <div class="icon">
+                        <img src="src/img/icons/coach.png" alt="">
+                    </div>
+
+                    <div class="name">
+                        <div class="nameEffect">
+                            <img src="src/img/team-nick-effect.png" alt="">
+                        </div>
+
+                        <span>Lazy Sensei</span>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-lg-8">
+            </div>
+        </div>
+    </div>
+</section>
