9fdf4f6bb2ec722344938b51cab8b97f2b3ad18f
unknown <israel-victor@bol.com.br>
2022-10-25 22:36:34 -0300

    added playerChar area; added playerChar area images

--- includes/components/player.php
+++ includes/components/player.php
@@ -12,8 +12,8 @@
 
 
             <!-- PLAYER NAME 1 -->
-            <div class="col-lg-3">
-                <div class="playerIcon active">
+            <div class="col-xl-4">
+                <div class="playerIcon active" data-id="player1">
 
                     <div class="icon">
                         <img src="src/img/icons/top.png" alt="">
@@ -30,7 +30,7 @@
                 </div>
 
                 <!-- PLAYER NAME 2 -->
-                <div class="playerIcon">
+                <div class="playerIcon" data-id="player2">
 
                     <div class="icon">
                         <img src="src/img/icons/jg.png" alt="">
@@ -47,7 +47,7 @@
                 </div>
 
                 <!-- PLAYER NAME 3 -->
-                <div class="playerIcon">
+                <div class="playerIcon" data-id="player3">
 
                     <div class="icon">
                         <img src="src/img/icons/mid.png" alt="">
@@ -64,7 +64,7 @@
                 </div>
 
                 <!-- PLAYER NAME 4 -->
-                <div class="playerIcon">
+                <div class="playerIcon" data-id="player4">
 
                     <div class="icon">
                         <img src="src/img/icons/adc.png" alt="">
@@ -81,7 +81,7 @@
 
 
                 <!-- PLAYER NAME 5 -->
-                <div class="playerIcon">
+                <div class="playerIcon" data-id="player5">
 
                     <div class="icon">
                         <img src="src/img/icons/sup.png" alt="">
@@ -98,7 +98,7 @@
                 </div>
 
                 <!-- PLAYER NAME 6 -->
-                <div class="playerIcon">
+                <div class="playerIcon" data-id="player6">
 
                     <div class="icon">
                         <img src="src/img/icons/coach.png" alt="">
@@ -115,7 +115,198 @@
                 </div>
             </div>
 
-            <div class="col-lg-8">
+            <div class="col-xl-8">
+
+                <div class="team">
+
+                    <!-- PLAYER CHAR 1 -->
+                    <div class="player active" id="player1">
+                        <div class="titleArea">
+                            <h3>SooH Biscoit0</h3>
+                        </div>
+
+                        <div class="playerChar active">
+                            <div class="statsArea">
+                                <div class="charImg">
+                                    <img src="src/img/char/top/poppy.png">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="playerChar">
+                            <div class="statsArea">
+                                <div class="charImg">
+                                    <img src="src/img/char/top/ornn.png">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="playerChar">
+                            <div class="statsArea">
+                                <div class="charImg">
+                                    <img src="src/img/char/top/camile.png">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- PLAYER CHAR 2 -->
+                    <div class="player" id="player2">
+                        <div class="titleArea">
+                            <h3>SooH Croco</h3>
+                        </div>
+
+                        <div class="playerChar active">
+                            <div class="statsArea">
+                                <div class="charImg">
+                                    <img src="src/img/char/jg/leesin.png">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="playerChar">
+                            <div class="statsArea">
+                                <div class="charImg">
+                                    <img src="src/img/char/jg/trundle.png">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="playerChar">
+                            <div class="statsArea">
+                                <div class="charImg">
+                                    <img src="src/img/char/jg/hecarim.png">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- PLAYER CHAR 3 -->
+                    <div class="player" id="player3">
+                        <div class="titleArea">
+                            <h3>SooH Vonor</h3>
+                        </div>
+
+                        <div class="playerChar active">
+                            <div class="statsArea">
+                                <div class="charImg">
+                                    <img src="src/img/char/mid/fizz.png">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="playerChar">
+                            <div class="statsArea">
+                                <div class="charImg">
+                                    <img src="src/img/char/mid/syndra.png">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="playerChar">
+                            <div class="statsArea">
+                                <div class="charImg">
+                                    <img src="src/img/char/mid/vex.png">
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <!-- PLAYER CHAR 4 -->
+                    <div class="player" id="player4">
+                        <div class="titleArea">
+                            <h3>SooH Greed</h3>
+                        </div>
+
+                        <div class="playerChar active">
+                            <div class="statsArea">
+                                <div class="charImg">
+                                    <img src="src/img/char/adc/tristana.png">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="playerChar">
+                            <div class="statsArea">
+                                <div class="charImg">
+                                    <img src="src/img/char/adc/aphelios.png">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="playerChar">
+                            <div class="statsArea">
+                                <div class="charImg">
+                                    <img src="src/img/char/top/ornn.png">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- PLAYER CHAR 5 -->
+                    <div class="player" id="player5">
+                        <div class="titleArea">
+                            <h3>SooH Lari</h3>
+                        </div>
+
+                        <div class="playerChar active">
+                            <div class="statsArea">
+                                <div class="charImg">
+                                    <img src="src/img/char/sup/yuumi.png">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="playerChar">
+                            <div class="statsArea">
+                                <div class="charImg">
+                                    <img src="src/img/char/sup/rakan.png">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="playerChar">
+                            <div class="statsArea">
+                                <div class="charImg">
+                                    <img src="src/img/char/sup/seraphine.png">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- PLAYER CHAR 6 -->
+                    <div class="player" id="player6">
+                        <div class="titleArea">
+                            <h3>Lazy Sensei</h3>
+                        </div>
+
+                        <div class="playerChar active">
+                            <div class="statsArea">
+                                <div class="charImg">
+                                    <img src="src/img/char/top/ornn.png">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="playerChar">
+                            <div class="statsArea">
+                                <div class="charImg">
+                                    <img src="src/img/char/top/ornn.png">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="playerChar">
+                            <div class="statsArea">
+                                <div class="charImg">
+                                    <img src="src/img/char/top/ornn.png">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     </div>
