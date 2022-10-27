3bb86608497cf8656224e1a17ec30271db474b36
unknown <israel-victor@bol.com.br>
2022-10-27 01:34:54 -0300

    added collapse button on header

--- includes/components/header.php
+++ includes/components/header.php
@@ -1,25 +1,29 @@
+<header>
     <div class="container-fluid container-xxl">
         <div class="row">
             <div class="col-lg-12">
-                <header>
 
-                    <div class="brandImg">
-                        <a href="#">
-                            <img src="src/img/logo.png" alt="">
-                        </a>
-                    </div>
+                <div class="brandImg">
+                    <a href="#">
+                        <img src="src/img/logo.png" alt="">
+                    </a>
+                </div>
 
-                    <nav>
-                        <ul class="navbarArea">
-                            <li><a href="#" class="active"> Home </a></li>
-                            <li><a href="#"> Jogadores </a></li>
-                            <li><a href="#"> Sobre </a></li>
-                            <li><a href="#"> Entre no time </a></li>
-                        </ul>
-                    </nav>
+                <div class="toggle">
+                    <div class="hamburger"></div>
+                </div>
 
-                </header>
+                <nav>
+                    <ul class="navbarArea">
+                        <li><a href="#" class="active"> Home </a></li>
+                        <li><a href="#"> Jogadores </a></li>
+                        <li><a href="#"> Sobre </a></li>
+                        <li><a href="#"> Entre no time </a></li>
+                    </ul>
+                </nav>
+
             </div>
 
         </div>
     </div>
+</header>
