837c70cf565b2d4e72a26f1311a166377d448f58
unknown <israel-victor@bol.com.br>
2022-10-26 23:03:42 -0300

    added comments in player.js

--- dist/js/players.js
+++ dist/js/players.js
@@ -1,25 +1,24 @@
-const player = document.querySelectorAll('.player')
-const playerIcon = document.querySelectorAll('.playerIcon')
+const player = document.querySelectorAll('.player') // get all elements with .player class
+const playerIcon = document.querySelectorAll('.playerIcon') // get all elementis with .playerIcon class
 
 
-for (let i = 0; i < playerIcon.length; i++) {
+for (let i = 0; i < playerIcon.length; i++) { // separates all elements with .playerIcon based on number of items
     playerIcon[i].addEventListener('click', function () {
         for (let i = 0; i < player.length; i++) {
             player[i].className = "player";
         }
 
-        document.getElementById(this.dataset.id).className = 'player active'
+        document.getElementById(this.dataset.id).className = 'player active' // on click in a playerIcon item, add classes from id equals data id
 
         for (let i = 0; i < playerIcon.length; i++) {
             playerIcon[i].className = 'playerIcon';
         }
         this.className = 'playerIcon active'
 
-        const playerChar = document.querySelectorAll('.player.active .playerChar')
+        const playerChar = document.querySelectorAll('.player.active .playerChar') // get all elements with .player.active .playerChar after click in a playerIcon item
 
         for (let i = 0; i < playerChar.length; i++) {
             playerChar[i].addEventListener('mouseover', function () {
-                // console.log('batatinha')
                 for (let i = 0; i < playerChar.length; i++) {
                     playerChar[i].className = 'playerChar'
                 }
@@ -29,11 +28,10 @@ for (let i = 0; i < playerIcon.length; i++) {
     })
 }
 
-const playerChar = document.querySelectorAll('.player.active .playerChar')
+const playerChar = document.querySelectorAll('.player.active .playerChar') // get all elements with .player.active .playerChar before click in a playerIcon item
 
 for (let i = 0; i < playerChar.length; i++) {
     playerChar[i].addEventListener('mouseover', function () {
-        // console.log('batatinha')
         for (let i = 0; i < playerChar.length; i++) {
             playerChar[i].className = 'playerChar'
         }
