6253d6bbf1d57cf83a43fbcee9c35721865efe69
unknown <israel-victor@bol.com.br>
2022-11-01 20:40:35 -0300

    added about.php

---
+++ includes/components/about.php
@@ -0,0 +1,29 @@
+<section class="aboutArea">
+
+    <div class="container-xxl container-fluid">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="aboutContent">
+                    <div class="titleArea">
+                        <small>SOBRE</small>
+                        <h1>Shadow of Hell</h1>
+                    </div>
+
+                    <div class="textArea">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent at dolor faucibus, cursus nibh sit amet, blandit ex. Integer tincidunt nunc diam, vel faucibus nulla dignissim sit amet. Phasellus ut molestie turpis. Nunc id lacus ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate laoreet finibus. Mauris ut dui a tortor cursus imperdiet.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent at dolor faucibus, cursus nibh sit amet, blandit ex. Integer tincidunt nunc diam, vel faucibus nulla dignissim sit amet. Phasellus ut molestie turpis. Nunc id lacus ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate laoreet finibus. Mauris ut dui a tortor cursus imperdiet.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent at dolor faucibus, cursus nibh sit amet, blandit ex. Integer tincidunt nunc diam, vel faucibus nulla dignissim sit amet. Phasellus ut molestie turpis. Nunc id lacus ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate laoreet finibus. Mauris ut dui a tortor cursus imperdiet.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-6">
+                <div class="imgArea">
+                    <img src="src/img/about-image.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</section>
