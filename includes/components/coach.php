e8dde4dbc6b7881f2daacb99e13a2275ffe26ac9
unknown <israel-victor@bol.com.br>
2022-11-02 17:39:36 -0300

    added coach.php

---
+++ includes/components/coach.php
@@ -0,0 +1,32 @@
+<section class="coachArea">
+    <div class="container-xxl container-fluid">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="imgArea">
+                    <img src="src/img/challenger.png" alt="">
+                </div>
+            </div>
+            <div class="col-lg-6">
+
+                <div class="titleArea">
+                    <small>Nosso coach</small>
+                    <h2>Lazy Sensei</h2>
+                </div>
+
+                <div class="textArea">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque velit deserunt nihil perferendis nesciunt quibusdam dolorum exercitationem quos, dicta quas cumque, quo impedit recusandae, mollitia cum qui eligendi eveniet enim explicabo praesentium ab optio fugiat! Vero totam aut illum harum.</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque velit deserunt nihil perferendis nesciunt quibusdam dolorum exercitationem quos, dicta quas cumque, quo impedit recusandae, mollitia cum qui eligendi eveniet enim explicabo praesentium ab optio fugiat! Vero totam aut illum harum.</p>
+
+                    <div class="titleArea">
+                        <h4>Gostaria de um coach pro seu time? Entre em contato pelo discord clicando no botão!</h4>
+                    </div>
+
+                    <a href="#" class="buttonStyle">
+                        Saiba mais
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
